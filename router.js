607514584c96630f81aafee6093b86be6fc4ba46
vinicius falconi <vinicius.falconi@ahgora.com.br>
2022-01-18 10:19:11 -0300

    adjustments for heroku

--- router.js
+++ router.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const { saveInterval, editInterval, deleteAll, getPauses } = require('./src/pauses')
 
+router.get('/', (req, res) => {
+    res.json({message: 'ta up'})
+})
+
 router.post('/pause', saveInterval)
 
 router.patch('/pause/:id', editInterval)
